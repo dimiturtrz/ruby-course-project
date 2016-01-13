268540f35336f56d93eef161a4b1b068b74119c0
dimiturtrz <dimitar.trz@gmail.com>
2016-01-13 19:50:09 +0200

    another bucket of tests for the piece base functionality, mostly the checks. Very useful

--- piece_base_spec.rb
+++ piece_base_spec.rb
@@ -9,6 +9,53 @@ describe Piece do
     end
   end
 
+  describe "Piece moevment checks" do
+    let(:board) { Board.new }
+    let(:piece) { Queen.new [:e, 4], :white }
+    
+    describe "#direction_check" do
+      it "should check for diagonal direction movement correctly" do
+        expect(piece.direction_check 4, 2, 2, :diagonal).to be true
+      end
+
+      it "should fail to go straight with diagonal direction" do
+        expect(piece.direction_check 4, 2, 2, :straight).to be false
+      end
+    end
+
+    describe "#limits_check" do
+      it "should be able to go further than one square with the limit off" do
+        expect(piece.limits_check 4, 2, 2, false).to be true
+      end
+
+      it "shouldn't be able to go beyond the one square limit" do
+        expect(piece.limits_check 4, 2, 2, true).to be false
+      end
+
+      it "should be able to go to adjacent square with the limit on" do
+        expect(piece.limits_check 4, 3, 3, true).to be true
+      end
+    end
+    
+    describe "#get_squares_to_pass" do
+      it "should get the right squares" do
+        expect(piece.get_squares_to_pass(4, 2, 2)).to eq([[:d, 3], [:c, 2]])
+      end
+    end
+
+    describe "#obstructions_check" do
+      it "should return the last square" do
+        expect(piece.obstructions_check(4, 1, 1, board).to_s).to eq("♟ 2")
+      end
+    end
+
+    describe "#obstructions_check" do
+      it "should return false if there's a piece in the way" do
+        expect(piece.obstructions_check(4, 0, 0, board)).to be false
+      end
+    end
+  end
+
   describe SingularPiece do
     let(:singular_piece_class) { Class.new{ include SingularPiece } }
 
